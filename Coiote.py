e0cfe7b1f1c12c1528f739ae31292701a829f07a
Gabriel Santos <gabrielboy.oliversantos@gmail.com>
2018-09-11 00:04:46 -0300

    Delete Coiote.py

--- Coiote.py
+++
@@ -1,9 +0,0 @@
-from tkinter import filedialog
-
-TIPO, TIME_STAMP, LONGITUDE, LATITUDE, ALTITUDE, BPM, N_PASSOS = list(range(7))
-mensagem = []
-
-with open(filedialog.askopenfilename(title = "Abrir", filetypes = [("Text files", "*.txt")])) as arquivo:
-	pass
-
-print(N_PASSOS)
