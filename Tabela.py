119e88f8efe697bc2613aa6ae73bb7304797caef
Gabriel Santos <gabrielboy.oliversantos@gmail.com>
2018-09-15 01:02:24 -0300

    Delete Tabela.py

--- Tabela.py
+++
@@ -1,8 +0,0 @@
-def selecionar(lista, tipoDaMensagem, dado = None):
-	selecionados = []
-
-	for mensagem in lista:
-		if mensagem[0] == tipoDaMensagem:
-			selecionados.append(mensagem)
-
-	return selecionados
